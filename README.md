c331459c254ebac178245ae8598f5eeb709ea6b0
henrychan990805 <henrychan990805@gmail.com>
2024-05-23 19:25:56 -0700

    edited flask app

--- README.md
+++
@@ -1,1 +0,0 @@
-# simple_viz_app
--- app.py
+++ app.py
@@ -1,26 +1,52 @@
 from flask import Flask, render_template, jsonify
 from sqlalchemy import create_engine, text, inspect
+import psycopg2
 
 app = Flask(__name__)
-engine=create_engine('sqlite:///database.db', echo=True)
-# engine=create_engine('postgresql://user:pass@localhost:5432/database.db')
+DB_NAME = 'database'
+DB_USER = 'postgres'
+DB_PASSWORD = 'postgres'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+
+def get_data_from_database(dataset):
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT * FROM {dataset}')
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return data
 
 @app.route("/")
 def home():
-    # https://flask.palletsprojects.com/en/3.0.x/quickstart/#rendering-templates
     return render_template('index.html')
 
+@app.route('/state_data')
+def data():
+    data = get_data_from_database('state_data')
+    return data
+
+@app.route('/county_data')
+def county_data():
+    data = get_data_from_database('county_data')
+    return data
+
+@app.route('/metropolitan_data')
+def county_data():
+    data = get_data_from_database('metropolitan_data')
+    return data
+
 @app.route('/data')
-def get_data(): 
-    query=text('''
-               SELECT * 
-               FROM data_table
-               ''')
-    conn=engine.connect()
-    results=conn.execute(query)
-    conn.close()
-    results=[tuple(row[1:]) for row in results]
-    return jsonify(results)
+def data():
+    data = get_data_from_database('employment_data')
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
